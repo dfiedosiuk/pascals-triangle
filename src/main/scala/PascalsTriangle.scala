323ddff20b69c6a311cef5a314b3d0ca184033f2
Daniel <d.fiedosiuk@adform.com>
2022-04-03 21:45:45 +0200

    Added function printTriangle for better presentation

--- src/main/scala/PascalsTriangle.scala
+++ src/main/scala/PascalsTriangle.scala
@@ -2,16 +2,17 @@
 object PascalsTriangle extends App {
 
   def create(n: Int): Seq[IndexedSeq[Int]] = {
-    val triangle =
-    for {
-      row <- 0 until n
-    } yield {
-      val line = for {
-        order <- 0 to row
+    val triangle = {
+      for {
+        row <- 0 until n
       } yield {
-        countValue(row, order)
+        val line = for {
+          order <- 0 to row
+        } yield {
+          countValue(row, order)
+        }
+        line
       }
-      line
     }
     triangle
   }
@@ -23,6 +24,22 @@ object PascalsTriangle extends App {
       value /= (i + 1)
     }
     value
+  }
+
+  def printTriangle(triangle: Seq[IndexedSeq[Int]]) = {
+    val maxLength = triangle.last.max.toString.length
+    triangle.foldLeft(triangle) { (triangle, row) =>
+      val rowToPrint = row
+        .map {
+          case c if (c.toString.length < maxLength) =>
+            " " * (maxLength - c.toString.length) + c.toString
+          case c => c.toString
+        }
+      val rowCentered = " " * (triangle.last.length - rowToPrint.length) +: rowToPrint
+      rowCentered.mkString(" ").foreach(print)
+      println()
+      triangle
+    }
   }
 
 }
---
+++ src/main/scala/untitled.sc
@@ -0,0 +1,3 @@
+val numbers = List(99, 1, 10, 5 , 12)
+numbers.sorted
+numbers.head.toString.length
